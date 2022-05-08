de6ff7184590affc0288ddf5b310d7fa7b0158c7
Jocelyn Vega <40295780+CXL58APj@users.noreply.github.com>
2022-05-08 17:29:39 +0800

    added navigation not  finished

---
+++ mobile_app/app/src/main/java/com/example/meally_app/DashboardActivity.java
@@ -0,0 +1,26 @@
+package com.example.meally_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class DashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavView);
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.fragmentContainerView);
+        NavController navController = navHostFragment.getNavController();
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(R.id.dashboardFragment, R.id.mapFragment, R.id.favoritesFragment, R.id.profileFragment).build();
+        NavigationUI.setupActionBarWithNavController(this, navController,appBarConfiguration);
+    }
+}
--- mobile_app/app/src/main/java/com/example/meally_app/SigninActivity.java
+++ mobile_app/app/src/main/java/com/example/meally_app/SigninActivity.java
@@ -1,8 +1,13 @@
 package com.example.meally_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SigninActivity extends AppCompatActivity {
 
@@ -10,5 +15,10 @@ public class SigninActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
+        getSupportActionBar().hide();
+//        NavController navigationController = Navigation.findNavController(this, R.id.navHostFragment);
+//        NavigationUI.setupActionBarWithNavController(this, navigationController);
+//        NavController navController1 = Navigation.findNavController(this, R.id.navHostFragment);
+//        NavigationUI.setupActionBarWithNavController(this, navController1);
     }
 }
